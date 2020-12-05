77201bdb74fa6e59d1aaa588cfa6f67c2da69ecb
Hayk <hayk.hayr3@gmail.com>
2020-12-05 21:48:45 +0400

    Add project files.

---
+++ Less03_task04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Less03_task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMW auto = new BMW_X5();
+
+            Console.WriteLine(auto.Drive(80));
+
+            Console.ReadKey();
+        }
+    }
+
+    class BMW
+    {
+        public virtual string Drive(ushort speed)
+        {
+            return "I'm a BMW auto and I run with speed " + speed + " km/h";
+        }
+    }
+
+    class BMW_X5 : BMW
+    {
+        public override string Drive(ushort speed)
+        {
+            return "I'm a BMW X5 auto and I run with speed " + speed + " km/h";
+        }
+    }
+}
